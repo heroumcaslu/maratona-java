865d45305535543d1ce3765db978a71a1150833f
Lucas H. Silva <lucas_herom@hotmail.com>
2020-03-08 17:01:26 -0300

    Aula 42 Metodos Estaticos

---
+++ src/com/devdojo/modificadorestatico/Car.java
@@ -0,0 +1,52 @@
+package com.devdojo.modificadorestatico;
+
+public class Car {
+
+	// Velocidade limite = 240km/h
+	private String name;
+	private double maxSpeed;
+	private static double speedLimit = 240;
+
+	public Car(String name, double maxSpeed) {
+		super();
+		this.name = name;
+		this.maxSpeed = maxSpeed;
+	}
+	
+	public Car() {
+		
+		
+		
+	}
+	
+	public void print() {
+		
+		System.out.println(this.name+": velocidade máxima = "+this.maxSpeed+", velocidade limite = "+Car.speedLimit);
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(double maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	public double getSpeedLimit() {
+		return speedLimit;
+	}
+
+	public void setSpeedLimit(double speedLimit) {
+		Car.speedLimit = speedLimit;
+	}
+
+}
---
+++ src/com/devdojo/modificadorestatico/test/CarTest.java
@@ -0,0 +1,20 @@
+package com.devdojo.modificadorestatico.test;
+
+import com.devdojo.modificadorestatico.Car;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car c1 = new Car("BMW", 280);
+		Car c2 = new Car("Audi", 275);
+		Car c3 = new Car("Mercedes", 230);
+		
+		c1.print();
+		c2.print();
+		c3.print();
+
+	}
+
+}
