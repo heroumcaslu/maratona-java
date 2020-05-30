2c186e7e2cf44a5ab044c8b77c0729207d224162
Lucas H. Silva <lucas_herom@hotmail.com>
2020-05-30 08:24:18 -0300

    Aula 89: Expressões regulares pt. 1

---
+++ src/com/devdojo/regularexpressions/RegEx.java
@@ -0,0 +1,26 @@
+package com.devdojo.regularexpressions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegEx {
+	public static void main(String[] args) {
+		
+		//Pattern, matcher
+		String regex = "ab";
+		//Não reutiliza caracteres já utilizados, por exemplo aba em abababa encontra a posição 0 e 4
+		String txt = "abaaaabaaabaaab";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(txt);
+		System.out.println("texto:  "+txt);
+		System.out.println("indice: 0123456789");
+		System.out.println("expressão: "+matcher.pattern());
+		System.out.println("Posições encontradas");
+		while(matcher.find()) {
+			
+			System.out.println(matcher.start()+" ");
+			
+		}
+		
+	}
+}
