72609264765e85bee1682a224d1b7594694c5a08
Lucas <lucas_herom@hotmail.com>
2020-10-01 07:29:34 -0300

    sobrecarga de métodos

---
+++ src/com/devdojo/sobrecargametodos/test/EmployeeTest.java
@@ -0,0 +1,16 @@
+package com.devdojo.sobrecargametodos.test;
+
+import com.devdojo.sobrecargametodos.Employee;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee employee = new Employee();
+		employee.setCpf("15515151551");
+		employee.setName("Zezinho");
+		employee.setSalary(1200);
+		
+	}
+
+}
